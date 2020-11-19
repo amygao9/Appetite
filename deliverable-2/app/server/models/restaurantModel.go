ef9c364c9d830f5165057faf550380c3857102d5
Shayan Khalili <shayankhalilimoghaddam@gmail.com>
2020-11-18 21:22:39 -0500

    changed Rating attribute of Restaurant modedl to float to represent decimal ratings such as 4.5. Added NumRatings attribute to for number of ratings.

--- deliverable-2/app/server/models/restaurantModel.go
+++ deliverable-2/app/server/models/restaurantModel.go
@@ -18,7 +18,8 @@ type Restaurant struct {
 	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	YelpID     string             `json:"yelpid" bson:"yelpid"`
 	Name       string             `json:"name" bson:"name"`
-	Rating     int                `json:"rating" bson:"rating"`
+	Rating     float64            `json:"rating" bson:"rating"`
+	NumRatings int                `json:"numratings" bson:"numratings"`
 	ImageURL   []string           `json:"imageURL,omitempty" bson:"imageURL,omitempty"`
 	Location   string             `json:"location" bson:"location"`
 	Address    string             `json:"address" bson:"address"`
