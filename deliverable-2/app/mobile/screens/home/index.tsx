7e716e9875b8c90cfe325894e524d692f310ae83
Joshua Chua <winwin.chua@gmail.com>
2020-11-19 19:58:06 -0500

    fixed spacing of home page

--- deliverable-2/app/mobile/screens/home/index.tsx
+++ deliverable-2/app/mobile/screens/home/index.tsx
@@ -1,9 +1,11 @@
 import React, {useRef} from 'react';
-import { StyleSheet, SafeAreaView, View, Button} from 'react-native';
+import { StyleSheet, SafeAreaView, View, Button, Dimensions} from 'react-native';
 import * as Icon from '@expo/vector-icons'
 import Swiper from 'react-native-deck-swiper';
 import {Card, CircleButton} from "../../components";
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+
+const { height } = Dimensions.get('window')
 
 const Home = ({navigation}) => {
     const useSwiper = useRef(null)
@@ -11,46 +13,57 @@ const Home = ({navigation}) => {
     const OnClickLike = () => useSwiper.current.swipeRight()
 
     return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.header}> 
-          <CircleButton name="x" Icon = {Icon.Feather}
-            color="#FF0E83" 
-            // onPress={() => console.log("preferences pressed 2")} 
-            onPress={() => navigation.navigate('Preferences')}
-          />
-        </View>
-
-        <View style={styles.swiper}> 
-          <Swiper
-              ref={useSwiper}
-              cards={photoCards}
-              renderCard={card => <Card card={card} />}
-              onSwipedRight ={() => {console.log('swiped right')}}
-              onSwipedLeft={() => {console.log('swiped left')}}
-              onSwipedAll={() => {console.log('finished stack')}}
-              infinite = {true}
-              verticalSwipe = {false}
-              backgroundColor={'#191A1D'}
-              stackSize= {2}>
-          </Swiper>
-        </View>
-
-        <View style={styles.footer}>
+      <SafeAreaView style={styles.container}>
+          <View style={styles.header}> 
             <CircleButton name="x" Icon = {Icon.Feather}
-            color="#FF0E83" onPress={OnClickDislike}
+              color="#FF0E83" 
+              // onPress={() => console.log("preferences pressed 2")} 
+              onPress={() => navigation.navigate('Preferences')}
             />
-            <CircleButton name="heart" Icon = {Icon.Entypo}
-            color="#6F05D6" onPress={superlike}
+
+            <CircleButton name="x" Icon = {Icon.Feather}
+              color="#FF0E83" 
+              // onPress={() => console.log("preferences pressed 2")} 
+              onPress={() => navigation.navigate('Preferences')}
             />
-            <CircleButton name="like" Icon = {Icon.SimpleLineIcons}
-            color="#00CC99" onPress={OnClickLike}
+
+            <CircleButton name="x" Icon = {Icon.Feather}
+              color="#FF0E83" 
+              // onPress={() => console.log("preferences pressed 2")} 
+              onPress={() => navigation.navigate('Preferences')}
             />
-        </View>
-    </SafeAreaView>
+          </View>
+
+          <View style={styles.swiper}> 
+            <Swiper
+                ref={useSwiper}
+                cards={photoCards}
+                renderCard={card => <Card card={card} />}
+                onSwipedRight ={() => {console.log('swiped right')}}
+                onSwipedLeft={() => {console.log('swiped left')}}
+                onSwipedAll={() => {console.log('finished stack')}}
+                infinite = {true}
+                verticalSwipe = {false}
+                stackSize= {2}>
+            </Swiper>
+          </View>
+
+          <View style={styles.footer}>
+              <CircleButton name="x" Icon = {Icon.Feather}
+              color="#FF0E83" onPress={OnClickDislike}
+              />
+              <CircleButton name="heart" Icon = {Icon.Entypo}
+              color="#6F05D6" onPress={superlike}
+              />
+              <CircleButton name="like" Icon = {Icon.SimpleLineIcons}
+              color="#00CC99" onPress={OnClickLike}
+              />
+          </View>
+      </SafeAreaView>
     )
 }
 
-const dislike = (navigation) => {
+const dislike = () => {
     console.log("pressed x button");
   }
   const like = () => {
@@ -75,36 +88,26 @@ const photoCards = [
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: '#191A1D'
-    },
-    text: {
-      textAlign: "center",
-      fontSize: 50,
-      backgroundColor: "transparent"
+      backgroundColor: '#b132bf'
     },
     header: {
-      position: 'absolute',
-      top:0,
-      flexDirection: "row",
-      justifyContent: "space-between"
+      flex: 4, 
+      flexDirection: "row", 
+      justifyContent: 'space-around', 
+      alignItems: 'flex-end', 
+      backgroundColor: '#c23237',
     },
     swiper: {
-      flex: 1, 
-      justifyContent: 'center'
+      flex: 36, 
+      backgroundColor: '#3289bf',
     },
     footer: {
-      position: 'absolute',
-      bottom:0,
-      backgroundColor: 'transparent',
+      flex: 4, 
+      backgroundColor: '#32bf57',
       flexDirection: "row",
-      justifyContent: "space-evenly",
-      paddingTop: 20,
-      paddingBottom: 70
-    },
-    stretch: {
-      resizeMode: 'stretch',
+      justifyContent: 'center',
+      alignItems: 'flex-end', 
     }
   });
 export default Home;
