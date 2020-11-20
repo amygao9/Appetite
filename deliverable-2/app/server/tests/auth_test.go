94593581112b3ff826ca1901b10d663273711242
Poplar Wang <wang.poplar@gmail.com>
2020-11-20 15:49:36 -0500

    Breaking up auth tests

--- deliverable-2/app/server/tests/auth_test.go
+++ deliverable-2/app/server/tests/auth_test.go
@@ -12,23 +12,38 @@ import (
 
 func TestHashing(t *testing.T) {
 	testPasswords := [3]string{"TestPassword1", "KevinBad", "?P@GQ2Jb?kcDWr4m"}
+	var hashedPasswords [3]string
 
 	for index, password := range testPasswords {
 		hashed, err := auth.HashPassword(password)
 		if err != nil {
 			t.Fatal(err)
 		}
+		hashedPasswords[index] = hashed
+	}
 
-		// Password matches hash
-		checkCorrectPassword := auth.CheckPasswordHash(testPasswords[index], hashed)
-		if !checkCorrectPassword {
-			t.Errorf("Hash and decryption did not match when expected")
+	t.Run("Test correct passwords", testCorrectPassword(testPasswords, hashedPasswords))
+	t.Run("Test wrong passwords", testWrongPassword(testPasswords, hashedPasswords))
+}
+
+func testCorrectPassword(testPasswords[3] string, hashedPasswords[3] string) func(*testing.T) {
+	return func(t *testing.T) {
+		for index, password := range testPasswords {
+			checkCorrectPassword := auth.CheckPasswordHash(password, hashedPasswords[index])
+			if !checkCorrectPassword {
+				t.Errorf("Hash and decryption did not match when expected")
+			}
 		}
+	}
+}
 
-		// Password does not match hash
-		checkWrongPassword := auth.CheckPasswordHash("Wrong", hashed)
-		if checkWrongPassword {
-			t.Errorf("Hash and decryption matched when not expected")
+func testWrongPassword(testPasswords[3] string, hashedPasswords[3] string) func(*testing.T) {
+	return func(t *testing.T) {
+		for index, _ := range testPasswords {
+			checkWrongPassword := auth.CheckPasswordHash("Wrong", hashedPasswords[index])
+			if checkWrongPassword {
+				t.Errorf("Hash and decryption matched when not expected")
+		}
 		}
 	}
 }
@@ -38,52 +53,61 @@ func TestTokens(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	port, portExists := os.LookupEnv("PORT")
+	readPort, portExists := os.LookupEnv("PORT")
 	if !portExists {
 		t.Fatal("Env vars missing")
 	}
+	port = readPort
 
-	client := &http.Client{}
+	client = &http.Client{}
 
-	// Request without token authentication
-	noTokenReq, err := http.NewRequest("GET", "http://localhost:" + port +"/restaurant", nil)
-	if err != nil {
-        t.Fatal(err)
-	}
-	noTokenResp, err := client.Do(noTokenReq)
-	if err != nil {
-        t.Fatal(err)
-	}
-	noTokenRespData, err := ioutil.ReadAll(noTokenResp.Body)
-    if err != nil {
-        t.Fatal(err)
-	}
-	const noAuth = "Invalid auth token"
-	if string(noTokenRespData) != noAuth {
-        t.Errorf("Router returned unexpected body: got %v", string(noTokenRespData))
-	}
-	
-	// Request with token authentication
-	token, err := auth.CreateToken("test@test.com")
-	if err != nil {
-        t.Fatal(err)
-	}
-	bearer := "Bearer " + token.AccessToken
+	t.Run("Test request without token", testAuthNoToken())
+	t.Run("Test request with valid token", testAuthWithToken())
+}
 
-	tokenReq, err := http.NewRequest("GET", "http://localhost:" + port +"/restaurant", nil)
-	if err != nil {
-        t.Fatal(err)
+func testAuthNoToken() func(*testing.T) {
+	return func(t *testing.T) {
+		noTokenReq, err := http.NewRequest("GET", "http://localhost:" + port +"/restaurant", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		noTokenResp, err := client.Do(noTokenReq)
+		if err != nil {
+			t.Fatal(err)
+		}
+		noTokenRespData, err := ioutil.ReadAll(noTokenResp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(noTokenRespData) != "Invalid auth token" {
+			t.Errorf("Router returned unexpected body: got %v", string(noTokenRespData))
+		}
 	}
-	tokenReq.Header.Set("Authorization", bearer)
-	tokenResp, err := client.Do(tokenReq)
-	if err != nil {
-        t.Fatal(err)
-	}
-	tokenRespData, err := ioutil.ReadAll(tokenResp.Body)
-    if err != nil {
-        t.Fatal(err)
-	}
-	if string(tokenRespData) == noAuth {
-        t.Errorf("Request was not authenticated")
+}
+
+func testAuthWithToken() func(*testing.T) {
+	return func(t *testing.T) {
+		token, err := auth.CreateToken("test@test.com")
+		if err != nil {
+			t.Fatal(err)
+		}
+		bearer := "Bearer " + token.AccessToken
+
+		tokenReq, err := http.NewRequest("GET", "http://localhost:" + port +"/restaurant", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tokenReq.Header.Set("Authorization", bearer)
+		tokenResp, err := client.Do(tokenReq)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tokenRespData, err := ioutil.ReadAll(tokenResp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(tokenRespData) == "Invalid auth token" {
+			t.Errorf("Request was not authenticated")
+		}
 	}
 }
