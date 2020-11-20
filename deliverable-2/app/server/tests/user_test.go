bd0859bc544937945318d81273db81f140df4afb
Poplar Wang <wang.poplar@gmail.com>
2020-11-20 15:52:06 -0500

    Breaking up user tests

--- deliverable-2/app/server/tests/user_test.go
+++ deliverable-2/app/server/tests/user_test.go
@@ -17,89 +17,99 @@ func TestUsers(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	port, portExists := os.LookupEnv("PORT")
+	readPort, portExists := os.LookupEnv("PORT")
 	if !portExists {
 		t.Fatal("Env vars missing")
 	}
+	port = readPort
 
-	client := &http.Client{}
+	client = &http.Client{}
 
-	// Test authentication fail
-	authDataInvalid, err := json.Marshal(map[string]interface{}{
-		"Email": "user@test.com",
-		"Password": "NotThePassword",
-	})
-	if err != nil {
-        t.Fatal(err)
-	}
+	t.Run("Test invalid login", testInvalidLogin())
+	t.Run("Test valid login", testValidLogin())
+}
 
-	authReqFail, err := http.NewRequest("POST", "http://localhost:" + port +"/user/auth", bytes.NewBuffer(authDataInvalid))
-	if err != nil {
-        t.Fatal(err)
+func testInvalidLogin() func(*testing.T) {
+	return func(t *testing.T) {
+		authDataInvalid, err := json.Marshal(map[string]interface{}{
+			"Email": "user@test.com",
+			"Password": "NotThePassword",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	
+		authReqFail, err := http.NewRequest("POST", "http://localhost:" + port + "/user/auth", bytes.NewBuffer(authDataInvalid))
+		if err != nil {
+			t.Fatal(err)
+		}
+		authReqFail.Header.Set("Content-Type", "application/json")
+	
+		authRespFail, err := client.Do(authReqFail)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer authRespFail.Body.Close()
+	
+		bodyBytes, err := ioutil.ReadAll(authRespFail.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		if bodyString != "Invalid password" {
+			t.Errorf("Auth did not fail despite invalid password")
+		}
 	}
-	authReqFail.Header.Set("Content-Type", "application/json")
+}
 
-	authRespFail, err := client.Do(authReqFail)
-	if err != nil {
-        t.Fatal(err)
-	}
-	defer authRespFail.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(authRespFail.Body)
-	if err != nil {
-        t.Fatal(err)
-	}
-	bodyString := string(bodyBytes)
-	if bodyString != "Invalid password" {
-		t.Errorf("Auth did not fail despite invalid password")
-	}
-
-	// Test authentication pass
-	authDataValid, err := json.Marshal(map[string]interface{}{
-		"Email": "user@test.com",
-		"Password": "TestPassword1",
-	})
-	if err != nil {
-        t.Fatal(err)
-	}
-
-	authReqPass, err := http.NewRequest("POST", "http://localhost:" + port +"/user/auth", bytes.NewBuffer(authDataValid))
-	if err != nil {
-        t.Fatal(err)
-	}
-	authReqPass.Header.Set("Content-Type", "application/json")
-
-	authRespPass, err := client.Do(authReqPass)
-	if err != nil {
-        t.Fatal(err)
-	}
-	authRespData, err := ioutil.ReadAll(authRespPass.Body)
-    if err != nil {
-        t.Fatal(err)
-	}
-	var tokenRet auth.Tokens
-	err = json.Unmarshal(authRespData, &tokenRet)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bearer := "Bearer " + tokenRet.AccessToken
-	const noAuth = "Invalid auth token"
-
-	checkAuthReq, err := http.NewRequest("GET", "http://localhost:" + port +"/restaurant", nil)
-	if err != nil {
-        t.Fatal(err)
-	}
-	checkAuthReq.Header.Set("Authorization", bearer)
-	checkAuthResp, err := client.Do(checkAuthReq)
-	if err != nil {
-        t.Fatal(err)
-	}
-	checkAuthRespData, err := ioutil.ReadAll(checkAuthResp.Body)
-    if err != nil {
-        t.Fatal(err)
-	}
-	if string(checkAuthRespData) == noAuth {
-        t.Errorf("Request was not authenticated")
+func testValidLogin() func(*testing.T) {
+	return func(t *testing.T) {
+		authDataValid, err := json.Marshal(map[string]interface{}{
+			"Email": "user@test.com",
+			"Password": "TestPassword1",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	
+		authReqPass, err := http.NewRequest("POST", "http://localhost:" + port + "/user/auth", bytes.NewBuffer(authDataValid))
+		if err != nil {
+			t.Fatal(err)
+		}
+		authReqPass.Header.Set("Content-Type", "application/json")
+	
+		authRespPass, err := client.Do(authReqPass)
+		if err != nil {
+			t.Fatal(err)
+		}
+		authRespData, err := ioutil.ReadAll(authRespPass.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var tokenRet auth.Tokens
+		err = json.Unmarshal(authRespData, &tokenRet)
+		if err != nil {
+			t.Fatal(err)
+		}
+	
+		bearer := "Bearer " + tokenRet.AccessToken
+		const noAuth = "Invalid auth token"
+	
+		checkAuthReq, err := http.NewRequest("GET", "http://localhost:" + port + "/restaurant", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		checkAuthReq.Header.Set("Authorization", bearer)
+		checkAuthResp, err := client.Do(checkAuthReq)
+		if err != nil {
+			t.Fatal(err)
+		}
+		checkAuthRespData, err := ioutil.ReadAll(checkAuthResp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(checkAuthRespData) == noAuth {
+			t.Errorf("Request was not authenticated")
+		}
 	}
 }
