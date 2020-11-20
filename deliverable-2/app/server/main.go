36bb5c0bbfd5cee209a09891d00bfbc20399e94f
rishabluthra <rishabjsluthra@gmail.com>
2020-11-19 22:53:54 -0800

    disable .env file for production

--- deliverable-2/app/server/main.go
+++ deliverable-2/app/server/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"backapp/api"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
 	}
 
 	api.RouterInit()
