ee9156402419a50d02fafac7c5e306eb3e0d6934
Joshua Chua <winwin.chua@gmail.com>
2020-11-20 22:12:31 -0500

    added swiping api calls

--- deliverable-2/app/mobile/api/restaurantAPI.ts
+++ deliverable-2/app/mobile/api/restaurantAPI.ts
@@ -3,28 +3,17 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import env from "../env";
 import client from "./axios";
 
-// export const apiGetRestaurants = async (categories, radius) => {
-//   try {
-//     const authToken = await AsyncStorage.getItem("authToken");
-//     const restaurants = await client.post(env.apiUrl + 'restaurant', {
-//                   "categories": categories,
-//                   "lat": 43.661282922175914,
-//                   "lng": -79.39409611053878,
-//                   "radius": radius
-//                 }, {headers: {"Authorization" : `Bearer ${authToken}`}});
 export const apiGetRestaurants = async (preferences, searchRadius) => {
   try {
     const authToken = await AsyncStorage.getItem("authToken");
 
+    //user location is hardcoded at 1 King's College Circle (to be updated for future deliverables)
     const requestBody = {
       "categories": preferences,
       "lat": 43.661282922175914,
       "lng": -79.39409611053878, 
       "radius": searchRadius
     }
-
-    console.log('preferences to send: ' + preferences)
-    console.log('search radius to send: ' + searchRadius)
 
     const restaurants = await client.post(env.apiUrl + 'restaurant', requestBody, {headers: {"Authorization" : `Bearer ${authToken}`}});
       
@@ -36,17 +25,36 @@ export const apiGetRestaurants = async (preferences, searchRadius) => {
     throw err;
   }
 }
+
 export const apiGetDetails = async (id) => {
   try {
     const authToken = await AsyncStorage.getItem("authToken");
 
     const details = await client.get(env.apiUrl + 'restaurant/'+id, {headers: {"Authorization" : `Bearer ${authToken}`}});
-      
+
     if (!details || typeof details.data === 'string') {
       throw 'Restaurants not found';
     }
     console.log(details.data)
     return details.data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const apiSwipeOnRestaurant = async (restaurantID, weight) => {
+  try {
+    const authToken = await AsyncStorage.getItem("authToken");
+
+    const res = await client.put(env.apiUrl + 'restaurant/swipe/' + restaurantID, {"weight": weight}, {headers: {"Authorization" : `Bearer ${authToken}`}});
+      
+    if (!res || res.data.length > 0) {
+      throw 'Unable to record swipe with weight ' + weight + ' on restaurant ID ' + restaurantID + '.';
+    }
+
+    console.log('Successfully recorded swipe with weight ' + weight + ' on restaurant ID ' + restaurantID + '.');
+
+    //return restaurants.data;
   } catch (err) {
     throw err;
   }
--- deliverable-2/app/mobile/screens/home/index.tsx
+++ deliverable-2/app/mobile/screens/home/index.tsx
@@ -3,17 +3,13 @@ import { StyleSheet, SafeAreaView, View, Button, Dimensions, Image} from 'react-
 import * as Icon from '@expo/vector-icons'
 import Swiper from 'react-native-deck-swiper';
 import {Card, CircleButton} from "../../components";
-import {apiGetRestaurants} from "../../api/restaurantAPI";
+import {apiGetRestaurants, apiSwipeOnRestaurant} from "../../api/restaurantAPI";
 import colors from '../../constants/Colors';
 const { height } = Dimensions.get('window')
 
 const Home = ({navigation}) => {
     const useSwiper = useRef(null)
-    const OnClickDislike = () => useSwiper.current.swipeLeft()
-    const OnClickLike = () => useSwiper.current.swipeRight()
-
     const [isLoading, setLoading] = useState(true);
-
     const [restaurantCards, setCards] = useState([]);
     const [preferences, setPreferences] = useState([])
     const [searchRadius, setSearchRadius] = useState(1.5)
@@ -53,15 +49,39 @@ const Home = ({navigation}) => {
       }
     } 
 
-    function updatePreferences(preferences, radius){
-
-      console.log('PREFERENCES after navigating: ' + preferences)
-      console.log('RADIUS after navigating: ' + radius)
-                        
+    const updatePreferences = (preferences, radius) => {       
       setSearchRadius(radius);
       setPreferences(preferences);
       fetchRestaurants(preferences, radius);
     } 
+
+    const onClickDislike = () => {
+      useSwiper.current.swipeLeft();
+    }
+    const onClickLike = () => {
+      useSwiper.current.swipeRight();
+    }
+
+    const recordLeftSwipe = (index) => {
+      console.log('dislike action: ' + index);
+      recordSwipe(restaurantCards[index].id, -1)
+    }
+
+    const recordRightSwipe = (index) => {     
+      console.log('like action: ' + index);
+      recordSwipe(restaurantCards[index].id, 1)
+    }
+
+    const recordSwipe = async (restaurantID, weight) => {
+      try {
+        await apiSwipeOnRestaurant(restaurantID, weight);
+      }
+      catch(err) {
+        if (err.message === "auth invalid") {
+          navigation.navigate("Auth");
+        }
+      }
+    }
 
     return (
       
@@ -102,9 +122,9 @@ const Home = ({navigation}) => {
             <Swiper
                 ref={useSwiper}
                 cards={restaurantCards}
-                renderCard={card => <Card card={card} navigation = {navigation}/>}
-                onSwipedRight ={() => {console.log('swiped right')}}
-                onSwipedLeft={() => {console.log('swiped left')}}
+                renderCard={(card) => <Card card={card} navigation = {navigation}/>}
+                onSwipedRight ={(index) => {recordRightSwipe(index)}}
+                onSwipedLeft={(index) => {recordLeftSwipe(index)}}
                 onSwipedAll={() => {console.log('finished stack')}}
                 backgroundColor={colors.darkGray}
                 infinite = {true}
@@ -114,12 +134,11 @@ const Home = ({navigation}) => {
           </View> )}
           
           <View style={styles.footer}>
-            
               <CircleButton name="x" Icon = {Icon.Feather}
-              color={colors.pink} onPress={OnClickDislike}
+              color={colors.pink} onPress={() => onClickDislike()}
               />
               <CircleButton name="like" Icon = {Icon.SimpleLineIcons}
-              color={colors.green} onPress={OnClickLike}
+              color={colors.green} onPress={() => onClickLike()}
               />
           </View>
       </SafeAreaView>
